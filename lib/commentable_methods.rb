92d6d1a4a13165bdb804e801aac6b172d7dd7544
Kiran Kumar S <skkiran@gmail.com>
2022-11-09 16:49:17 +0530

    fix for ruby 3.1 and rails 7.0

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -16,12 +16,12 @@ module Juixe
         end
 
         def define_role_based_inflection_3(role)
-          has_many "#{role.to_s}_comments".to_sym,
+          has_many "#{role.to_s}_comments".to_sym, nil,
                    has_many_options(role).merge(:conditions => { role: role.to_s })
         end
 
         def define_role_based_inflection_4(role)
-          has_many "#{role.to_s}_comments".to_sym,
+          has_many "#{role.to_s}_comments".to_sym, nil,
                    -> { where(role: role.to_s) },
                    has_many_options(role)
         end
@@ -53,9 +53,9 @@ module Juixe
             comment_roles.each do |role|
               define_role_based_inflection(role)
             end
-            has_many :all_comments, { :as => :commentable, :dependent => :destroy, class_name: 'Comment' }.merge(join_options)
+            has_many :all_comments, nil, **{ :as => :commentable, :dependent => :destroy, class_name: 'Comment' }.merge(join_options)
           else
-            has_many :comments, {:as => :commentable, :dependent => :destroy}.merge(join_options)
+            has_many :comments, nil, **{:as => :commentable, :dependent => :destroy}.merge(join_options)
           end
 
           comment_types.each do |role|
